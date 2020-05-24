b7aebfafd9cc7dd4b92cfd582f413d867aa23105
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-05-24 22:00:40 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -21,12 +21,23 @@ apt install nano -y
 apt install ruby -y
 gem install lolcat
 apt install git curl openssh openssl openssl-tool -y
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
+progress-bar() {
+  local duration=${1}
+
+
+    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
+    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+    clean_line() { printf "\r"; }
+
+  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
+      already_done; remaining; percentage
+      sleep 1
+      clean_line
+  done
+  clean_line
+}
+
 figlet -f small FINISHED !!! | lolcat
 echo
 echo -e "\e[1m Now Run bash termux...!!!"
