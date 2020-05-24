35ec202e5982a0d2c5a98f8b3a8d93b6040db8b4
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-05-25 00:34:48 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 clear
-echo "\e[1m;32m termux basic installation setup \e[0m\n"
+echo -e "\e[40;38;5;82m termux basic installation setup \e[0m"
 echo ""
 echo ""
-echo "Press Enter To Continue"
+echo -e "\e[101m Press Enter"
 read a1
 
 pkg install git 
@@ -35,7 +35,7 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [=======================================>] 100%
-printf "\rLOADING : [${_done// /\e[30;48;5;82m|\e[0m"}${_left// /}] ${_progress}%%"
+printf "\rLOADING : [${_done// /|}${_left// /}] ${_progress}%%"
 
 }
 
