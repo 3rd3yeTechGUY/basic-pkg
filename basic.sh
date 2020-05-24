32526633feae1cfedb43198b24d66fdc3ebbde34
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-05-24 22:47:37 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -34,7 +34,7 @@ function ProgressBar {
 
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
-# 1.2.1.1 Progress : [########################################] 100%
+# 1.2.1.1 Progress : [=======================================>] 100%
 printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
 
 }
