33a8a4e3582dda2bc40d13351d6df916f0694e81
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-05-24 23:02:15 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -35,7 +35,7 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [=======================================>] 100%
-printf "\rProgress : [${_done// /=/>}${_left// /-}] ${_progress}%%"
+printf "\rProgress : [${_done// />}${_left// /}] ${_progress}%%"
 
 }
 
@@ -51,7 +51,7 @@ do
 	sleep 0.1
 	ProgressBar ${number} ${_end}
 done
-echo ""
+printf ''
 
 figlet -f small FINISHED !!! | lolcat
 echo
