cfae692625fa902a74cfcfe24e59c32614e76148
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-05-25 12:05:41 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -4,8 +4,7 @@ clear
 echo -e "\e[40;38;5;82m TERMUX BASIC PACKAGE INSTALLATION SETUP \e[0m"
 echo ""
 echo ""
-echo -e "\e[101m Press Enter"
-read a1
+echo -e "\e[101m TECH COCHI \e[0m"
 xdg-open https://www.youtube.com/TechCochi?sub_confirmation=1
 
 echo -e "\e[101m Press Enter"
@@ -49,7 +48,7 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [=======================================>] 100%
-printf "\rLOADING : [${_done// /:}${_left// /}] ${_progress}%%"
+printf "\r"\e[101mLOADING" : [${_done// /:}${_left// /}] ${_progress}%%"
 
 }
 
